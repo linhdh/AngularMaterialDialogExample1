efd3d13af8e071c5590af2fa43f0cf73f04f32c8
Linh Dao <linhdh@live.com>
2024-02-25 09:14:08 +0700

    Add template modal

--- Angular-material-dialog-example1/src/app/app.component.ts
+++ Angular-material-dialog-example1/src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDialogModule, CommonModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-material-dialog-example1';
+
+  @ViewChild('dialogRef')
+  dialogRef!: TemplateRef<any>;
+
+  myFooList = ['Some Item', 'Item Second', 'Other In Row', 'What to write', 'Blah To Do']
+
+  constructor(public dialog: MatDialog) { }
+
+  openTempDialog() {
+    const myTempDialog = this.dialog.open(this.dialogRef, { data: this.myFooList });
+    myTempDialog.afterClosed().subscribe((res) => {
+
+      // Data back from dialog
+      console.log({ res });
+    });
+  }
 }
